ef8a92ddbdf8befa914fcf47dc29183e190a0afe
Domi <git@secnd.me>
2024-08-24 13:07:57 +0200

    Add comment for limit removal

--- src/JellySearch/Controllers/SearchController.cs
+++ src/JellySearch/Controllers/SearchController.cs
@@ -101,6 +101,7 @@ public class SearchController : ControllerBase
             // Get all query arguments to pass along to Jellyfin
             // Remove searchterm since we already searched
             // Remove sortby and sortorder since we want to display results as Meilisearch returns them
+            // Remove limit since we are requesting by specific IDs and don't want Jellyfin to remove some of them
             var query = this.Request.Query.Where(x =>
                 !string.Equals(x.Key, "searchterm", StringComparison.InvariantCultureIgnoreCase) &&
                 !string.Equals(x.Key, "sortby", StringComparison.InvariantCultureIgnoreCase) &&
